27bdad1a9df7bcef2a92ef5a81639c31e1ea1b19
Gevapo <geert@geertvanpoucke.be>
2016-03-11 13:33:38 +0100

    Chapter 02

---
+++ private/bin/load-fixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__.'/../src/bootstrap.php';
+
+
+echo '<br>..' . __DIR__; //D:\Development\workspaces\LearnToCode\doctrineBlog\private\bin
+
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+
+$loader = new Loader();
+$loader->loadFromDirectory(__DIR__.'/../src/Blog/DataFixtures');
+
+$purger = new ORMPurger();
+$executor = new ORMExecutor($entityManager, $purger);
+$executor->execute($loader->getFixtures());
